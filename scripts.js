ed3647d4aa0ee73b6dcf67a4b1704bbcc34f6a70
AntonyFB <antonyrbramwell@gmail.com>
2022-06-11 12:52:48 +0100

    Code function playRound

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ function computerPlay() {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
-    var computerResponse = getRandomInt(1,3);
+    const computerResponse = getRandomInt(1,3);
 
     if (computerResponse == 1) {
             text = ("Rock")
@@ -14,5 +14,43 @@ function computerPlay() {
     if (computerResponse == 3) {
             text = ("Scissors")
     }
-    console.log(text)
+    return(text);
+}
+
+function playRound(playerSelection, computerSelection) {
+    var playerSelection = prompt("Choose.");
+    var computerSelection = computerPlay();
+    if (playerSelection.toLowerCase() == "rock") {
+        if (computerSelection == "Rock") {
+            return("A tie!");
+        }
+        if (computerSelection == "Paper") {
+            return("You lose! Paper beats rock!");
+        }
+        if (computerSelection == "Scissors") {
+            return("You win! Rock beats scissors!");
+        }
+    }
+    if (playerSelection.toLowerCase() == "paper") {
+        if (computerSelection == "Rock") {
+            return("You win! Paper beats rock!");
+        }
+        if (computerSelection == "Paper") {
+            return("A tie!");
+        }
+        if (computerSelection == "Scissors") {
+            return("You lose! Scissors beats paper!");
+        }
+    }
+    if (playerSelection.toLowerCase() == "scissors") {
+        if (computerSelection == "Rock") {
+            return("You lose! Rock beats scissors!");
+        }
+        if (computerSelection == "Paper") {
+            return("You win! Scissors beats paper!");
+        }
+        if (computerSelection == "Scissors") {
+            return("A tie!");
+        }
+    }
 }
